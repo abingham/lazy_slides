d0701daebb5969d6c8a06165be9c9fb140109f4e
Austin Bingham <austin.bingham@gmail.com>
2011-12-10 13:49:42 +0100

    Added -F flag to fail when no search result is found.

--- lazy_slides/search.py
+++ lazy_slides/search.py
@@ -8,10 +8,13 @@ log = logging.getLogger(__name__)
 def search_photos(tags):
     for tag in tags:
         log.info('searching for images tagged with "{}"'.format(tag))
+
         photos = flickr.photos_search(tags=tag, per_page=1)
+
         if not photos:
             log.warning('No results for "{}"'.format(tag))
+            yield (tag, None)
         else:
             photo = photos[0]
             log.info('found photo: {}'.format(photo))
-            yield photo.getURL(size='Medium', urlType='source')
+            yield (tag, photo.getURL(size='Medium', urlType='source'))
--- lazy_slides/slides.py
+++ lazy_slides/slides.py
@@ -26,6 +26,11 @@ def parse_args():
         default='slides.beamer',
         metavar='F',
         help='The filename for the output.')
+    parser.add_argument(
+        '-F, --fail-on-missing',
+        dest='fail_on_missing',
+        action='store_true',
+        help='Fail when a tag generates no matches.')
 
     return parser.parse_args()
 
@@ -44,10 +49,25 @@ def run():
     init_logging(args.verbose)
 
     urls = search.search_photos(args.tags)
+
+    def filter_failures(match):
+        if match[1] is None:
+            if args.fail_on_missing:
+                raise ValueError(
+                    'No matching images for tag "{}"'.format(
+                        match[0]))
+            else:
+                return False
+        return True
+    urls = list(filter(filter_failures, urls))
+
+    tags = (u[0] for u in urls)
+    urls = (u[1] for u in urls)
+
     filenames = download.download(urls)
 
     with open(args.output, 'w') as outfile:
-        generate.generate_slides(args.tags, filenames, outfile)
+        generate.generate_slides(tags, filenames, outfile)
 
 if __name__ == '__main__':
     run()
