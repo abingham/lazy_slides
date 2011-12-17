a2f1ee3518e1f3ba11681266bd32d4e57e70e001
Austin Bingham <austin.bingham@gmail.com>
2011-12-17 20:01:39 +0100

    downloaded images are now stamped with a UUID.

--- lazy_slides/download.py
+++ lazy_slides/download.py
@@ -1,12 +1,22 @@
 import logging
+import os
 import urllib
+from urllib.parse import urlparse
+import uuid
 
 
 log = logging.getLogger(__name__)
 
 def download(urls):
     for idx, url in enumerate(urls):
-        filename = 'slide_image_{}.jpg'.format(idx)
+        parsed = urlparse(url)
+        filename = os.path.split(parsed.path)[1]
+        filename_comps = os.path.splitext(filename)
+        filename = '{}_{}{}'.format(
+            filename_comps[0],
+            uuid.uuid4(),
+            filename_comps[1])
+
         log.info('Downloading {} to {}'.format(
                 url, filename))
 
