1d31c532b4a7c4627b4b8e2dfd183fc8bce8ea21
Austin Bingham <austin.bingham@gmail.com>
2011-12-31 14:50:48 +0100

    We now run concurrently.

--- lazy_slides/slides.py
+++ lazy_slides/slides.py
@@ -1,4 +1,5 @@
 import argparse
+import concurrent.futures as futures
 import importlib
 import logging
 import sys
@@ -57,6 +58,13 @@ def parse_args():
         default=200,
         metavar='INT',
         help='The height of the slide image.')
+    parser.add_argument(
+        '-w, --workers',
+        dest='num_workers',
+        type=int,
+        default=0,
+        metavar='INT',
+        help='The number of worker threads or processes to use.')
 
     return parser.parse_args()
 
@@ -101,16 +109,24 @@ def init_search_function(search_function):
 
     search.search_function = getattr(mod, func_name)
 
-def process_tag(tag, args):
-    url = search.search_photos(tag)
-    if url is None:
-        if args.fail_on_missing:
-            raise ValueError(
-                'No matching images for tag "{}"'.format(tag))
-        return
+def process_tag(tag):
+    '''Search, download, and convert a single image based on a single
+    tag.
 
-    in_filename = download.download(url)
-    out_filename = manipulation.convert(in_filename)
+    :param tag: The tag to search on.
+    :param throw_on_failure: Whether an exception should be thrown if
+      when no match is found.
+
+    :return: A tuple (tag, filename) defining the match. If there was
+       no match, filename will be None.
+    '''
+    url = search.search_photos(tag)
+    out_filename = None
+
+    if url is not None:
+        in_filename = download.download(url)
+        out_filename = manipulation.convert(in_filename)
+
     return (tag, out_filename)
 
 def main():
@@ -118,7 +134,33 @@ def main():
     init_logging(args.verbose)
     init_search_function(args.search_function)
 
-    tags, filenames = zip(*map(lambda tag: process_tag(tag, args), args.tags))
+    # Determine how many workers we should use. If no number is
+    # specified, make one per tag.
+    num_workers = args.num_workers
+    if num_workers == 0:
+        num_workers = len(args.tags)
+    log.info('Using {} workers'.format(num_workers))
+
+    # Run the searches/downloads concurrently.
+    executor = futures.ThreadPoolExecutor(num_workers)
+    results = executor.map(
+        process_tag,
+        args.tags)
+
+    def filter_failure(rslt):
+        '''Filter out failed searches, throwing if we're configured to
+        do so.
+        '''
+        if rslt[1] is None:
+            if args.fail_on_missing:
+                raise ValueError('No match for {}'.format(rslt[0]))
+            return False
+        else:
+            return True
+
+    # Filter out match failures
+    results = filter(filter_failure, results)
+    tags, filenames = zip(*results)
 
     # Generate the slideshow.
     log.info('Writing output to file {}'.format(args.output))
