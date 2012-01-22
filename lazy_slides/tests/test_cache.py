65c9735ec09e7432b4f0d63727b07ce12723ee0c
Austin Bingham <austin.bingham@gmail.com>
2012-01-22 15:17:38 +0100

    updated cache tests.

--- lazy_slides/tests/test_cache.py
+++ lazy_slides/tests/test_cache.py
@@ -1,39 +1,63 @@
 import unittest
 
-from lazy_slides.cache import Cache
+from lazy_slides.cache import open_cache
 
 from lazy_slides.tests.util import (remove, temp_file)
 
 class CacheTest(unittest.TestCase):
 
     def setUp(self):
-        self.db_file = 'test.db'
-        remove(self.db_file)
+        self.db_file = ':memory:'
+    #     remove(self.db_file)
 
-    def tearDown(self):
-        remove(self.db_file)
+    # def tearDown(self):
+    #     remove(self.db_file)
 
     def test_get(self):
         tag = 'tag'
         filename = 'test_file'
 
-        cache = Cache(self.db_file)
-        self.assertEqual(cache.get(tag), None)
+        with open_cache(self.db_file, 1000) as cache:
+            self.assertEqual(cache.get(tag), None)
 
-        with temp_file(filename):
-            cache.set(tag, filename)
+            with temp_file(filename):
+                cache.set(tag, filename)
 
-            self.assertEqual(cache.get(tag), filename)
+                self.assertEqual(cache.get(tag), filename)
 
     def test_get_missing_file(self):
         tag = 'tag'
         filename = 'test_file'
 
-        cache = Cache(self.db_file)
+        with open_cache(self.db_file, 100) as cache:
 
-        with temp_file(filename):
-            cache.set(tag, filename)
+            with temp_file(filename):
+                cache.set(tag, filename)
 
-            self.assertEqual(cache.get(tag), filename)
+                self.assertEqual(cache.get(tag), filename)
 
-        self.assertEqual(cache.get(tag), None)
+            self.assertEqual(cache.get(tag), None)
+
+    def test_size(self):
+        with open_cache(self.db_file, 1000) as cache:
+            self.assertEqual(cache.size(), 0)
+
+            for i in range(100):
+                cache.set(str(i), str(i))
+                self.assertEqual(cache.size(), i + 1)
+
+    def test_trim(self):
+        SIZE = 100
+
+        with open_cache(self.db_file, 1000) as cache:
+            for i in range(SIZE):
+                cache.set(str(i), str(i))
+
+            self.assertEqual(cache.size(), SIZE)
+
+            NEW_SIZE=40
+            cache.trim(NEW_SIZE)
+            self.assertEqual(cache.size(), NEW_SIZE)
+
+            cache.trim(cache.size() + 1)
+            self.assertEqual(cache.size(), NEW_SIZE)
