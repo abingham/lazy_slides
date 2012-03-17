88b3cf0265a89ca998c921685a32d9b0f77cc2da
Austin Bingham <austin.bingham@gmail.com>
2012-03-18 00:14:25 +0100

    Now using reportlab.

--- lazy_slides/generate.py
+++ lazy_slides/generate.py
@@ -1,3 +1,5 @@
+from reportlab.pdfgen import canvas
+
 def generate_slides(tags,
                     tag_map,
                     outfile,
@@ -9,17 +11,13 @@ def generate_slides(tags,
     :param outfile: The name of the file into which the results should
       be saved.
     '''
-    outfile.write('\\documentclass{beamer}\n')
-    outfile.write('\\usetheme{Copenhagen}\n')
-    outfile.write('\\begin{document}\n')
+    c = canvas.Canvas(outfile)
     for tag in tags:
-        outfile.write('\\begin{frame}\n')
-        outfile.write('\\frametitle{{{}}}'.format(tag))
-        outfile.write('\\begin{center}\n')
-        outfile.write('\\includegraphics[width={width}px, height={height}px]{{{filename}}}'.format(
-                width=args.image_width,
-                height=args.image_height,
-                filename=tag_map[tag]))
-        outfile.write('\\end{center}\n')
-        outfile.write('\\end{frame}\n')
-    outfile.write('\\end{document}\n')
+        c.drawImage(tag_map[tag],
+                    0,
+                    0,
+                    width=400,
+                    height=200)
+        c.showPage()
+    c.save()
+
